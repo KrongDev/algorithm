c29ce52ca916e1027902a765331f9611ce5fc1f4
Geon Lee <drik86421@gmail.com>
2024-04-29 19:53:31 +0900

    fix: Cording_test Programmers 42842

--- src/main/java/com/krong/cording_test/programmers/Test42842.java
+++ src/main/java/com/krong/cording_test/programmers/Test42842.java
@@ -5,26 +5,40 @@ public class Test42842 {
     //3의 배수여야한다.
     public int[] solution(int brown, int yellow) {
         int all = brown + yellow;
-        int a = 3;
-        int b = 0;
-        while (a < all) {
-            int mod = all%a;
-            if(mod ==0) {
-                b = all/a;
-                if((a-2) * (b-2) >= yellow)
-                    break;
-            }
-            a+=3;
+
+        int x = 0;
+        int y = 0;
+        for(int i = all; i > 0; i--) {
+            x = i;
+            y = all/i;
+            if(isYellowOk(x, y, yellow) && isAll(x, y, all)) break;
         }
-        if(a < b)
-            return new int[]{b, a};
-        else
-            return new int[]{a, b};
+
+        return makeAnswer(x, y);
+    }
+
+    private int mix(int x, int y) {
+        return x * y;
+    }
+
+    private boolean isAll(int x, int y, int all) {
+        return mix(x, y) == all;
+    }
+
+    private boolean isYellowOk(int x, int y, int yellow) {
+        return mix(x-2, y-2) == yellow;
+    }
+
+    private int[] makeAnswer(int x, int y) {
+        int[] answer = new int[2];
+        answer[0] = Math.max(x, y);
+        answer[1] = Math.min(x, y);
+        return answer;
     }
 
     public static void main(String[] args) {
         Test42842 solution = new Test42842();
-        int[] answer = solution.solution(18, 6);
+        int[] answer = solution.solution(10, 2);
         System.out.println(answer[0]);
         System.out.println(answer[1]);
     }
